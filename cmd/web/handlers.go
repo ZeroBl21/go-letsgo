1b915f6a6e6ad4ab4650d8cb4fd82ddabd9be26c
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-09 16:04:19 -0400

    feat(web/handlers.go): Implement custom validator on snippetCreatePost

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,6 +9,7 @@ import (
 	"unicode/utf8"
 
 	"github.com/ZeroBl21/go-letsgo/internal/models"
+	"github.com/ZeroBl21/go-letsgo/internal/validator"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -57,10 +58,10 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -79,31 +80,26 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 
 	form := snippetCreateForm{
-		Title:       title,
-		Content:     content,
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   title,
+		Content: content,
+		Expires: expires,
 	}
 
 	// Title
-	if strings.TrimSpace(title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	}
-	if utf8.RuneCountInString(title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 character long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title",
+		"This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title",
+		"This field cannot be be more than 100 characters long")
 
 	// Content
-	if strings.TrimSpace(content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
+	form.CheckField(validator.NotBlank(form.Content), "content",
+		"This field cannot be blank")
 
 	// Expires
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires",
+		"This field must be equal to 1, 7 or 365")
 
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.html", data)
