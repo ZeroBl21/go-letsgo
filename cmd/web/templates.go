e2b1c9498f0f6ddf44135d542c92fb69912b547a
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-16 14:21:51 -0400

    refactor(web/templates): Embedding template files

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"net/http"
 	"path/filepath"
 	"time"
 
 	"github.com/ZeroBl21/go-letsgo/internal/models"
+	"github.com/ZeroBl21/go-letsgo/ui"
 	"github.com/justinas/nosurf"
 )
 
@@ -35,7 +37,7 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 	if err != nil {
 		return nil, err
 	}
@@ -43,17 +45,16 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
-		if err != nil {
-			return nil, err
+		patterns := []string{
+			"html/base.html",
+			"html/partials/*.html",
+			page,
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
+		ts, err := template.
+			New(name).
+			Funcs(functions).
+			ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
