d31f3539ad16308d6a061cd5fda2e4ea6683af51
Darwin Perdomo <ZeroBl@hotmail.com>
2024-07-18 13:52:35 -0400

    feat(internal/models/mocks): Add mocks to user and snippets models

---
+++ internal/models/mocks/users.go
@@ -0,0 +1,32 @@
+package mocks
+
+import "github.com/ZeroBl21/go-letsgo/internal/models"
+
+type UserModel struct{}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	switch email {
+	case "dupe@example.com":
+		return models.ErrDuplicateEmail
+	default:
+		return nil
+	}
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	if email == "alice@example" && password == "pa$$word" {
+		return 1, nil
+	}
+
+	return 0, models.ErrNoRecord
+}
+
+func (m *UserModel) Exists(id int) (bool, error) {
+	switch id {
+	case 1:
+		return true, nil
+	default:
+
+		return false, nil
+	}
+}
